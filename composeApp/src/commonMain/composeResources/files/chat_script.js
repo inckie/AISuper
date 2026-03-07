2ec85598a74e46979e43252c2a97dadb3e978fea
Ink <i@am.com>
2026-03-07 04:10:24 -0500

    + better chat

--- composeApp/src/commonMain/composeResources/files/chat_script.js
+++ composeApp/src/commonMain/composeResources/files/chat_script.js
@@ -1,5 +1,39 @@
+var messages = [];
+
+function refreshMessages() {
+    var widgets = [];
+    for (var i = 0; i < messages.length; i = i + 1) {
+        var msg = messages[i];
+        var item = {
+            "type": "Text",
+            "text": msg
+        };
+        widgets.push(item);
+    }
+    var json = JSON.stringify(widgets);
+    setValue("messageList", json);
+}
+
+function initialize() {
+    messages.push("System: Welcome to Echo Chat!");
+    refreshMessages();
+}
+
 function process() {
     var input = getValue("input_field");
-    var result = "Echo: " + input;
-    setValue("result_text", result);
+    if (input == undefined) {
+        return;
+    }
+    if (input == "") {
+        return;
+    }
+
+    var uMsg = "You: " + input;
+    messages.push(uMsg);
+
+    var eMsg = "Echo: " + input;
+    messages.push(eMsg);
+
+    refreshMessages();
+    setValue("input_field", "");
 }
