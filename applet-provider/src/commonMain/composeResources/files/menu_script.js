32f5a4de123fad44a18698305354c057a9d19c11
Ink <i@am.com>
2026-06-06 21:40:02 -0400

    * cleanup applet features

--- applet-provider/src/commonMain/composeResources/files/menu_script.js
+++ applet-provider/src/commonMain/composeResources/files/menu_script.js
@@ -32,24 +32,11 @@ function initialize() {
     var features = getFeatures();
 
     var widgets = [];
-    var mediaHeaderAdded = false;
     for (var i = 0; i < features.length; i++) {
         var f = features[i];
         if (f.id === "menu") continue;
 
-        if (f.id === "radio" && !mediaHeaderAdded) {
-            widgets.push({
-                "type": "Text",
-                "text": "Media",
-                "classes": ["section_title"]
-            });
-            mediaHeaderAdded = true;
-        }
-
         var buttonText = f.name;
-        if (f.id === "radio") {
-            buttonText = "Radio Search";
-        }
 
         widgets.push({
             "type": "Button",
